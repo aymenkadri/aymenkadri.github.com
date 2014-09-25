dc4f7638bd08bf802812d9447b34e16281afa1f5
KADRIA <AKADRI@mediametrie.fr>
2014-09-25 18:07:14 +0200

    merge conflicts

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name aymenkadrigithubcomApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the aymenkadrigithubcomApp
+ */
+angular.module('aymenkadrigithubcomApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
