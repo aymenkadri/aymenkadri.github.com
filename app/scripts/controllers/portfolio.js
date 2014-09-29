992c7e729149452056e39bac50b7f055179c0c3d
KADRIA <AKADRI@mediametrie.fr>
2014-09-29 16:09:06 +0200

    Add track event for google analytics

--- app/scripts/controllers/portfolio.js
+++ app/scripts/controllers/portfolio.js
@@ -9,7 +9,7 @@
  */
 angular.module('aymenkadrigithubcomApp')
     .controller('PortfolioCtrl', function ($scope, $location, $window) {
-        $window._gaq.push(['_trackPageView', $location.path()]);
+        $window._gaq.push(['_trackEvent', $location.path(), "Click"]);
         $scope.awesomeThings = [
             'HTML5 Boilerplate',
             'AngularJS',
